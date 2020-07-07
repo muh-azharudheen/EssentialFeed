aaea9bf98fc42d8208842313ef8c8f254258437c
Muhammed Azharudheen <muhammed.azharudheen@nagarro.com>
2020-07-07 23:59:20 +0400

    crete the feed item model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Muhammed Azharudheen on 07/07/2020.
+//  Copyright © 2020 Nagarro. All rights reserved.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: String
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
