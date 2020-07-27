e47f1ebd8eb3f763fb6a94f474559d446ff9bf9c
Muhammed Azharudheen <muhammed.azharudheen@nagarro.com>
2020-07-28 00:38:41 +0400

    Guarantee we do not deliver a result (invoke the competion closure) after the `RemoteFeedLoader` has been deallocated.

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -84,6 +84,20 @@ class RemoteFeedLoaderTests: XCTestCase {
             client.complete(withStatusCode: 200, data: json)
         })
     }
+    
+    func test_load_doesNotDeliverResultsAfterSUTInstanceHasBeenDeallocated() {
+        let url = URL(string: "http://any-url.com")!
+        let client = HTTPClientSpy()
+        var sut: RemoteFeedLoader? = RemoteFeedLoader(url: url, client: client)
+        
+        var capturedResults = [RemoteFeedLoader.Result]()
+        sut?.load() { capturedResults.append($0) }
+        
+        sut = nil
+        client.complete(withStatusCode: 200, data: makeItemsJson(items: []))
+        
+        XCTAssertTrue(capturedResults.isEmpty)
+    }
 }
 
 // MARK: Helpers
