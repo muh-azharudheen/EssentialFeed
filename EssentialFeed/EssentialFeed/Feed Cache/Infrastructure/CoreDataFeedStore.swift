0124dcca26d4cae0132c03936f6ac158c2fb508e
Azhar <muhdazharudheen@gmail.com>
2021-12-25 16:14:31 +0400

    assertThatInsertOverridesPreviouslyInsertedCacheValues

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreDataFeedStore.swift
@@ -38,7 +38,7 @@ public final class CoreDataFeedStore: FeedStore {
         perform { context in
             do {
         
-                let managedCache = ManagedCache(context: context)
+                let managedCache = try ManagedCache.newUniqueInstance(in: context)
                 managedCache.timestamp = timeStamp
                 managedCache.feed = ManagedFeedImage.images(from: feed, in: context)
                 
@@ -103,6 +103,11 @@ private extension NSPersistentContainer {
 private class ManagedCache: NSManagedObject {
      @NSManaged var timestamp: Date
      @NSManaged var feed: NSOrderedSet
+    
+    static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
+        try find(in: context).map(context.delete)
+        return ManagedCache(context: context)
+    }
     
     static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
         let request = NSFetchRequest<ManagedCache>(entityName: entity().name!)
