061f630d24ea5c74041eda524a493a5da7cb074b
Azhar <muhdazharudheen@gmail.com>
2021-12-25 16:16:05 +0400

    Move CodableFeedStore to the new infrastructure folder

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CodableFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CodableFeedStore.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-public class CodableFeedStore: FeedStore {
+public final class CodableFeedStore: FeedStore {
     
     private struct CodableFeedImage: Codable {
         let id: UUID
