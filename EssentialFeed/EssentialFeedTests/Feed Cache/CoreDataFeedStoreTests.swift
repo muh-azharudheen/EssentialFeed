094d19950e5ade8dab7569239aaefe874fe194a7
Azhar <muhdazharudheen@gmail.com>
2021-12-25 11:41:32 +0400

    Make storeURL an explicit dependency so we can inject test-specific URLs (such as `/dev/null`) to avoid sharing state with production (and other tests!)

--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -54,9 +54,11 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     }
     
     // - MARK: Helpers
-     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
-         let sut = try! CoreDataFeedStore(bundle: Bundle(for: CoreDataFeedStore.self))
-         trackForMemmoryLeaks(sut, file: file, line: line)
-         return sut
-     }
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
+        let storeBundle = Bundle(for: CoreDataFeedStore.self)
+        let storeURL = URL(fileURLWithPath: "/dev/null")
+        let sut = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
+        trackForMemmoryLeaks(sut, file: file, line: line)
+        return sut
+    }
 }
